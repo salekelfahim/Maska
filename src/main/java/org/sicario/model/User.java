e80994d96c5fd265a513088968e67701fa5410be
salekelfahim <salekelfahim@gmail.com>
2024-10-23 11:07:24 +0100

    Adding User entity

--- src/main/java/org/sicario/model/User.java
+++ src/main/java/org/sicario/model/User.java
@@ -1,4 +1,15 @@
 package org.sicario.model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+
 }
